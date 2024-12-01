10cd8cce7a9200ecc2bc29512f6a05643427d1ae
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-02 04:15:35 +0530

    fix:add vercel analytics

--- src/ui/layout/Layout.jsx
+++ src/ui/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import { Outlet } from "react-router-dom"
 
+import {Analytics} from '@vercel/analytics/react'
 import Navbar from "$components/Navbar"
 import navList from "$utils/NavList"
 import Footer from "$components/Footer"
@@ -11,6 +12,7 @@ function Layout() {
       <Navbar navItems={navList} className={"rounded-[7px] uppercase"} />
       <Outlet />
       <Footer />
+      <Analytics/>
     </section>
   )
 }
