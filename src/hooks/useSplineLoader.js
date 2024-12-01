1b8079d1c9d0205c1fb73f6ad4bcc5b49cd571b0
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-02 03:28:44 +0530

    add splineLoader hook to track loading progress

---
+++ src/hooks/useSplineLoader.js
@@ -0,0 +1,14 @@
+import { progressAtom } from "$stores/loadingProgress"
+import { useSetAtom } from "jotai"
+import { useState } from "react"
+
+export function useSplineLoader() {
+  const x = ""
+  const [isLoading, setIsLoading] = useState(true)
+  const setProgress = useSetAtom(progressAtom)
+  const onLoad = () => {
+    setProgress(100)
+    setIsLoading(false)
+  }
+  return { isLoading, onLoad }
+}
