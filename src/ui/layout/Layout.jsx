a816fdc845573e0ea85747c31fcf1d5439c088b3
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-02 03:37:30 +0530

    fix:reomved extra spaces

--- src/ui/layout/Layout.jsx
+++ src/ui/layout/Layout.jsx
@@ -5,11 +5,7 @@ import { Outlet } from "react-router-dom"
 import Navbar from "$components/Navbar"
 import navList from "$utils/NavList"
 import Footer from "$components/Footer"
-import LoadingScreen from "$components/LoadingScreen"
-import { useSplineLoader } from "$hooks/useSplineLoader"
-
 function Layout() {
-
   return (
     <section className="grid grid-rows-[1fr_auto_auto] min-h-svh">
       <Navbar navItems={navList} className={"rounded-[7px] uppercase"} />
