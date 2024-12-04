17db48266aac9ddad158ee92ddf114c301a25203
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-04 13:57:59 +0530

    fix:refactor

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom'
-import Home from '../ui/pages/Home.jsx'
-import Layout from '../ui/layout/Layout.jsx'
-import ErrorPage from '../ui/pages/Error.jsx'
+import Home from '$ui/pages/Home.jsx'
+import Layout from '$ui/layout/Layout.jsx'
+import ErrorPage from '$ui/pages/Error.jsx'
 const routes = createBrowserRouter([
     {
         path : "/",
