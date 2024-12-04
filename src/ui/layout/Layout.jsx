e72a264bdb17cf9a34aa2838c570ae3587c164b1
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-04 06:20:20 +0530

    refactor

--- src/ui/layout/Layout.jsx
+++ src/ui/layout/Layout.jsx
@@ -2,18 +2,20 @@ import React from "react"
 
 import { Outlet } from "react-router-dom"
 
-import {Analytics} from '@vercel/analytics/react'
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "$components/Navbar"
 import navList from "$utils/NavList"
 import Footer from "$components/Footer"
 function Layout() {
   return (
-    <section className="grid grid-rows-[1fr_auto_auto] min-h-svh">
-      <Navbar navItems={navList} className={"rounded-[7px] uppercase"} />
-      <Outlet />
-      <Footer />
-      <Analytics/>
-    </section>
+    <>
+      <section className="grid grid-rows-[1fr_auto] min-h-svh overflow-x-hidden">
+        <Navbar navItems={navList} className={"rounded-[7px] uppercase"} />
+        <Outlet />
+        <Footer />
+      </section>
+      <Analytics />
+    </>
   )
 }
 
