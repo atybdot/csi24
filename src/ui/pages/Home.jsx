91616676e2e27213de2c5fe812468ba84c282a77
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-04 06:11:22 +0530

    add itroduction section

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Hero from '$ui/blocks/Hero'
 import Stats from '$ui/blocks/Stats'
+import Intro from '$ui/blocks/Intro'
 
 function Home() {
   return (
     <>
     <Hero/>
+    <Intro/>
     <Stats/>
     </>
 
