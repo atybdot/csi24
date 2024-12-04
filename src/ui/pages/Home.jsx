ca0b6e622269fa86c776bcce6ed19e83cc49a61b
atybdot <180611249+atybdot@users.noreply.github.com>
2024-12-04 13:56:19 +0530

    add faculty

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Hero from '$ui/blocks/Hero'
 import Stats from '$ui/blocks/Stats'
 import Intro from '$ui/blocks/Intro'
+import FacultySection from './Faculty'
 
 function Home() {
   return (
     <>
     <Hero/>
     <Intro/>
+    <FacultySection/>
     <Stats/>
     </>
 
